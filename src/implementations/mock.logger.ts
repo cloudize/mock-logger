b9d872cfb759bf0919cf5c72e461c787a537ead1
Erich Kuba <ekuba@appsolute.nz>
2020-11-24 04:42:43 +1300

    Implemented the upstream changes to the ILogger contract.  Bumped the version and published

--- src/implementations/mock.logger.ts
+++ src/implementations/mock.logger.ts
@@ -2,8 +2,8 @@ import { ILogger, LoggerMessageType, LoggerPayload } from '@apigames/logger';
 
 // eslint-disable-next-line import/prefer-default-export
 const MockLogger: ILogger = class {
-  // eslint-disable-next-line no-undef,no-unused-vars
-  static write = jest.fn(async (type: LoggerMessageType, message: string, payload?: LoggerPayload): Promise<void> => {})
+  // eslint-disable-next-line no-undef,no-unused-vars,max-len
+  static write = jest.fn(async (type: LoggerMessageType, message: string, payload?: LoggerPayload, date?: Date): Promise<void> => {})
 };
 
 export default MockLogger;
