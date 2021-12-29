2c6dabc210305e8fb33fb8301c58740a490e5c9b
Erich Kuba <erich.kuba@api.games>
2021-12-29 14:31:22 +1300

    Updated dependencies. Updated the license year. Bumped the version and published.

--- src/implementations/mock.logger.ts
+++ src/implementations/mock.logger.ts
@@ -1,9 +1,15 @@
+/* eslint-disable no-unused-vars */
 import { ILogger, LoggerMessageType, LoggerPayload } from '@apigames/logger';
 
 // eslint-disable-next-line import/prefer-default-export
 const MockLogger: ILogger = class {
-  // eslint-disable-next-line no-undef,no-unused-vars,max-len
-  static Write = jest.fn(async (type: LoggerMessageType, message: string, payload?: LoggerPayload, date?: Date): Promise<void> => {});
+  // eslint-disable-next-line no-undef
+  static Write = jest.fn(async (
+    type: LoggerMessageType,
+    message: string,
+    payload?: LoggerPayload,
+    date?: Date,
+  ): Promise<void> => {});
 };
 
 export default MockLogger;
